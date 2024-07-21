be90443e1d69675bed347d0a8bea0fcb9ecee05d
saul4797435 <Saul4797435@outlook.com>
2024-07-21 15:24:42 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
